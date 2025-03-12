7d0c7e3348011d87a7ba20d91013178eba9f9926
Fio <36188360+sopoour@users.noreply.github.com>
2025-03-12 16:27:55 +0100

    Butterfly animation (#3)
    * First draft of butterfly animation
    
    * First working version of random butterflies
    
    * Added rotation and all same butterflies
    
    * Animation stops when out of intersection
    
    * change size

--- src/components/sections/Hero/Hero.tsx
+++ src/components/sections/Hero/Hero.tsx
@@ -1,8 +1,112 @@
-import { FC, forwardRef } from 'react';
+import { forwardRef, useRef, useEffect } from 'react';
 import styles from './Hero.module.scss';
+import Image from 'next/image';
+import { gsap } from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { useIntersection, useInViewport } from '@mantine/hooks';
+
+gsap.registerPlugin(useGSAP);
+
+function random(min: number, max: number) {
+  if (max == null) {
+    max = min;
+    min = 0;
+  }
+  return Math.random() * (max - min) + min;
+}
+
+const butterFlyFiles = [
+  'assets/hero/butterfly_0.svg',
+  'assets/hero/butterfly_1.svg',
+  'assets/hero/butterfly_2.svg',
+  'assets/hero/butterfly_3.svg',
+  'assets/hero/butterfly_4.svg',
+  'assets/hero/butterfly_5.svg',
+  'assets/hero/butterfly_6.svg',
+  'assets/hero/butterfly_7.svg',
+];
 
 const Hero = forwardRef<HTMLDivElement>((props, ref) => {
-  return <div className={styles.background} ref={ref} />;
+  const butterflyRefs = useRef<Array<HTMLImageElement | null>>([null]);
+  const { ref: intersectionRef, entry } = useIntersection({
+    threshold: 0.75,
+  });
+
+  const animateProperty = (target: HTMLImageElement, prop: string, min: number, max: number) => {
+    gsap?.to(target, {
+      duration: random(2, 4),
+      [prop]: random(min, max),
+      ease: 'sine.inAndOut',
+      onComplete: animateProperty,
+      onCompleteParams: [target, prop, min, max],
+    });
+  };
+
+  const animateButterfly = (butterfly: HTMLImageElement, width: number, height: number) => {
+    const dx = width * 0.8;
+    const dy = height * 0.8;
+    const newX = random(0, dx);
+    const newY = random(0, dy);
+    const angle = Math.atan2(newY - butterfly.y, newX - butterfly.x) * (180 / Math.PI);
+
+    gsap.set(butterfly, { xPercent: -50, yPercent: -50 });
+
+    animateProperty(butterfly, 'scale', random(0.7, 1), random(0.5, 1));
+    gsap.to(butterfly, {
+      duration: random(2, 4),
+      x: newX,
+      y: newY,
+      rotation: angle,
+      ease: 'sine.inAndOut',
+      onComplete: () => animateButterfly(butterfly, width, height),
+    });
+  };
+
+  const startAnimations = () => {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    butterflyRefs.current.forEach((butterfly, index) => {
+      if (!butterfly) return;
+      animateButterfly(butterfly, width, height);
+    });
+  };
+
+  const stopAnimations = () => {
+    gsap.globalTimeline.clear();
+  };
+
+  useEffect(() => {
+    if (entry?.isIntersecting) {
+      startAnimations();
+    } else {
+      stopAnimations();
+    }
+  }, [entry?.isIntersecting]);
+
+  return (
+    <div className={styles.background} ref={ref}>
+      <div
+        ref={intersectionRef}
+        id="wrapperBirds"
+        style={{ position: 'relative', width: '100vw', height: '100vh', overflow: 'hidden' }}
+      >
+        {butterFlyFiles.map((butterfly, index) => (
+          <Image
+            key={index}
+            alt={`Butterfly ${index}`}
+            src={butterfly}
+            width={80}
+            height={80}
+            priority
+            ref={(ref) => {
+              butterflyRefs.current[index] = ref;
+            }}
+          />
+        ))}
+      </div>
+    </div>
+  );
 });
 
 Hero.displayName = 'Hero';
