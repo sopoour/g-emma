bcb6c3df726fb02a30f05dfc0b01964dda7aece1
sopoour <sophia.auer@googlemail.com>
2025-03-14 13:23:56 +0100

    ListRow added

--- src/components/sections/Live/Live.tsx
+++ src/components/sections/Live/Live.tsx
@@ -1,34 +1,73 @@
 import { Container, Flex, SimpleGrid, Text, Title } from '@mantine/core';
 import { FC } from 'react';
 import styles from './Live.module.scss';
+import LiveRow from './elements/LiveRow';
+
+const liveData = [
+  {
+    date: '2024-07-07T12:00:00.000Z',
+    eventType: 'Buskers Festival',
+    location: 'Braunschweig',
+    constellation: 'TRIO',
+  },
+  {
+    date: '2024-06-02T12:00:00.000Z',
+    eventType: 'Buskers Festival',
+    location: 'Braunschweig',
+    constellation: 'Band',
+  },
+];
 
 const Live: FC = () => {
   return (
-    <section>
-      <Container size={'lg'}>
-        <Flex direction={'row'} gap={'lg'}>
-          <Title size={'md'}>Upcoming Shows</Title>
-          <Flex direction={'column'} gap={'xl'} className={styles.liveGrid}>
-            <Flex direction={{ base: 'column', sm: 'row' }} gap={'md'} justify={'space-evenly'}>
-              <Flex direction={'column'}>
-                <Text fw={600}>02.06.2024</Text>
-                <Text>Buskers Festival</Text>
-              </Flex>
-              <Text>Braunschweig</Text>
-              <Text>TRIO</Text>
-            </Flex>
-            <Flex direction={'row'} gap={'md'} justify={'space-evenly'}>
-              <Flex direction={'column'}>
-                <Text fw={600}>02.06.2024</Text>
-                <Text>Buskers Festival</Text>
-              </Flex>
-              <Text>Braunschweig</Text>
-              <Text>TRIO</Text>
-            </Flex>
-          </Flex>
+    <Container
+      size={'md'}
+      component={'section'}
+      id="live"
+      style={{ display: 'flex', flexDirection: 'column', gap: '48px' }}
+    >
+      <Flex
+        direction={{ base: 'column', xs: 'row' }}
+        gap={'xl'}
+        justify={'space-between'}
+        align={{ base: 'center', xs: 'stretch' }}
+        style={{ width: '100%' }}
+      >
+        <Text size={'lg'} fw={700} ff={'BioRhyme'} style={{ whiteSpace: 'nowrap' }}>
+          Upcoming Shows
+        </Text>
+        <Flex
+          direction={'column'}
+          className={styles.liveGrid}
+          align={{ base: 'center', xs: 'stretch' }}
+        >
+          {liveData.map((live) => (
+            <LiveRow key={live.date} {...live} />
+          ))}
         </Flex>
-      </Container>
-    </section>
+      </Flex>
+
+      <Flex
+        direction={{ base: 'column', xs: 'row' }}
+        gap={'xl'}
+        align={{ base: 'center', xs: 'stretch' }}
+        justify={'space-between'}
+        style={{ width: '100%' }}
+      >
+        <Text size={'lg'} fw={700} ff={'BioRhyme'} style={{ whiteSpace: 'nowrap' }}>
+          Past Shows
+        </Text>
+        <Flex
+          direction={'column'}
+          className={styles.liveGrid}
+          align={{ base: 'center', xs: 'stretch' }}
+        >
+          {liveData.map((live) => (
+            <LiveRow key={live.date} {...live} />
+          ))}
+        </Flex>
+      </Flex>
+    </Container>
   );
 };
 
