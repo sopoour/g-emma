ae63e47915164c41c54488eb67e8260ff68d1c45
sopoour <sophia.auer@googlemail.com>
2025-04-24 23:24:14 +0200

    Ticket button to span

--- src/components/sections/Live/elements/LiveRow.tsx
+++ src/components/sections/Live/elements/LiveRow.tsx
@@ -49,7 +49,7 @@ const LiveRow: FC<Props> = ({
       <Grid.Col span={1} className={styles.liveRowCol}>
         {(ticketLink || ticketNotiz) &&
           (ticketLink ? (
-            <Button variant="outline" pb={'xs'} pt={'xs'} component="div">
+            <Button variant="outline" pb={'xs'} pt={'xs'} component="span">
               {date && new Date(date) >= new Date() ? 'Ticket' : 'Impressions'}
             </Button>
           ) : (
